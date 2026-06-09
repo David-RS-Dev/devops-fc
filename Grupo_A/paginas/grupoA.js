9bd80d66833530c00b0a243a3a50774537fd56c3
Stalyin <stalyin16ac@gmail.com>
2026-06-08 21:50:05 -0500

    CP: fix: actualizar id grupo A - Corea

--- Grupo_A/paginas/grupoA.js
+++ Grupo_A/paginas/grupoA.js
@@ -16,7 +16,7 @@ const equiposGrupoA = [
     jugadores: ["Hirving Lozano", "Edson Álvarez", "Santiago Giménez"],
   },
   {
-    id: "corea",
+    id: "coreadelsur",
     nombre: "Corea del Sur",
     badge: "AFC",
     imagen:
