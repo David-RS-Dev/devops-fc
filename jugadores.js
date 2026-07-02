bb1580afed3dcf47b8dfbd2a6e1eda629fdc3142
Stalyin <stalyin16ac@gmail.com>
2026-07-02 16:39:19 -0500

    CP: Agregar jugadores dinámicos de los grupos A y B

--- jugadores.js
+++ jugadores.js
@@ -1,30 +1,131 @@
 const cromosMundial = [];
 
-function crearCromoJugador(
-  id,
-  nombre,
-  pais,
-  posicion,
-  urlImagen,
-  urlBandera,
-  colorFondoHex,
-  goles,
-  partidos,
-  destacado,
-) {
-  return {
-    id: id,
-    nombre: nombre,
-    pais: pais,
-    posicion: posicion,
-    urlImagen: urlImagen,
-    urlBandera: urlBandera,
-    colorFondoHex: colorFondoHex,
+cromosMundial.push(
+  crearCromoJugador({
+    id: 1,
+    nombre: "Alphonso Davies",
+    pais: "Canadá",
+    posicion: "Defensa",
+    urlImagen:
+      "https://www.ole.com.ar/images/2023/06/11/b3CfoIUe__1290x650__1.jpg",
+    urlBandera:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Canada.svg/1280px-Flag_of_Canada.svg.png",
+    colorFondoHex: "#D80621",
     estadisticas: {
-      goles: goles,
-      partidos: partidos,
+      goles: 15,
+      partidos: 50,
     },
-    destacado: destacado,
+    destacado: true,
+    curiosidad: "Es considerado uno de los laterales más rápidos del mundo.",
+  }),
+
+  crearCromoJugador({
+    id: 2,
+    nombre: "Jonathan David",
+    pais: "Canadá",
+    posicion: "Delantero",
+    urlImagen:
+      "https://www.ole.com.ar/images/2026/06/18/NjjANbpLA_400x400__1.jpg",
+    urlBandera:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Canada.svg/1280px-Flag_of_Canada.svg.png",
+    colorFondoHex: "#D80621",
+    estadisticas: {
+      goles: 32,
+      partidos: 62,
+    },
+    destacado: true,
+    curiosidad: "Es el máximo goleador histórico de la selección canadiense.",
+  }),
+
+  crearCromoJugador({
+    id: 3,
+    nombre: "Granit Xhaka",
+    pais: "Suiza",
+    posicion: "Mediocampista",
+    urlImagen:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNIWrOICPkTUlUIUSFMgXcHiv3mIEmtbLTXLb6zKxCMQ&s=10",
+    urlBandera:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Switzerland.svg/1280px-Flag_of_Switzerland.svg.png",
+    colorFondoHex: "#D52B1E",
+    estadisticas: {
+      goles: 14,
+      partidos: 135,
+    },
+    destacado: true,
+    curiosidad:
+      "Ha sido capitán de Suiza durante varios torneos internacionales.",
+  }),
+
+  crearCromoJugador({
+    id: 4,
+    nombre: "Breel Embolo",
+    pais: "Suiza",
+    posicion: "Delantero",
+    urlImagen:
+      "https://www.ole.com.ar/images/2022/12/24/U_cYSOkts_720x0__1.jpg",
+    urlBandera:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Switzerland.svg/1280px-Flag_of_Switzerland.svg.png",
+    colorFondoHex: "#D52B1E",
+    estadisticas: {
+      goles: 18,
+      partidos: 72,
+    },
+    destacado: false,
+    curiosidad: "Debutó con la selección absoluta cuando tenía apenas 17 años.",
+  }),
+
+  crearCromoJugador({
+    id: 5,
+    nombre: "Akram Afif",
+    pais: "Qatar",
+    posicion: "Extremo",
+    urlImagen:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7Sj7vyn92j5_UnJ5mOGxb3iTWXvqIw1aHrQaK35MIhA&s=10",
+    urlBandera:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Flag_of_Qatar.svg/1280px-Flag_of_Qatar.svg.png",
+    colorFondoHex: "#8A1538",
+    estadisticas: {
+      goles: 33,
+      partidos: 112,
+    },
+    destacado: true,
+    curiosidad: "Fue el mejor jugador de la Copa Asiática 2023.",
+  }),
+
+  crearCromoJugador({
+    id: 6,
+    nombre: "Edin Džeko",
+    pais: "Bosnia y Herzegovina",
+    posicion: "Delantero",
+    urlImagen:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8rKZHadAOEXMBofwsYrk4qNUrdkLRlcZtTn_j2c4sNA&s=10",
+    urlBandera:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Flag_of_Bosnia_and_Herzegovina.svg/1280px-Flag_of_Bosnia_and_Herzegovina.svg.png",
+    colorFondoHex: "#002F6C",
+    estadisticas: {
+      goles: 67,
+      partidos: 140,
+    },
+    destacado: true,
+    curiosidad: "Es el máximo goleador histórico de Bosnia y Herzegovina.",
+  }),
+);
+
+function crearCromoJugador(datosJugador) {
+  return {
+    id: datosJugador.id,
+    nombre: datosJugador.nombre,
+    pais: datosJugador.pais,
+    posicion: datosJugador.posicion,
+    urlImagen: datosJugador.urlImagen,
+    urlBandera: datosJugador.urlBandera,
+    colorFondoHex: datosJugador.colorFondoHex,
+    estadisticas: {
+      goles: datosJugador.estadisticas.goles,
+      partidos: datosJugador.estadisticas.partidos,
+    },
+    destacado: datosJugador.destacado,
+    curiosidad: datosJugador.curiosidad,
   };
 }
 
@@ -49,6 +150,7 @@ function renderizarAlbum() {
       <p>${jugador.posicion}</p>
       <p>Goles: ${jugador.estadisticas.goles}</p>
       <p>Partidos: ${jugador.estadisticas.partidos}</p>
+      <p>${jugador.curiosidad}</p>
     `;
 
     contenedor.appendChild(card);
